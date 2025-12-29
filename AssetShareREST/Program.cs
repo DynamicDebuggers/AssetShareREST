960a48a0d3d8caebbb0a6a1217017f241a646738
Itsguo14 <maagaardlarsen@gmail.com>
2025-12-29 14:57:06 +0100

    Add JWT authentication and user secrets support
    Configured JWT bearer authentication and authorization in Program.cs, added UserSecretsId to the project file, and removed sensitive MongoDB connection info from appsettings.json. These changes improve security by managing secrets outside of source control and enabling token-based authentication.

--- AssetShareREST/Program.cs
+++ AssetShareREST/Program.cs
@@ -1,7 +1,12 @@
 using AssetShareLib;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
+// Tilføj lister 
 builder.Services.Configure<MongoDbSettings>(
     builder.Configuration.GetSection("MongoDb"));
 
@@ -13,11 +18,34 @@ builder.Services.AddScoped<BookingRepository>();
 
 builder.Services.AddControllers();
 
+// Token autentifikation
+var jwt = builder.Configuration.GetSection("Jwt");
+var key = Encoding.UTF8.GetBytes(jwt["Key"]!);
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            ValidIssuer = jwt["Issuer"],
+            ValidAudience = jwt["Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(key),
+            ClockSkew = TimeSpan.FromSeconds(30)
+        };
+    });
 
+builder.Services.AddAuthorization();
+
+// Brug Swagger
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+// Brug Cors
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll",
@@ -40,8 +68,8 @@ app.UseCors("AllowAll");
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
-
 app.Run();
